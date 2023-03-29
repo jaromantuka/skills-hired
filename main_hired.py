8c075b48315865baf6b005ad1e3c13901fb84b39
Olena Rudenko <olena.rudenko@gmail.com>
2023-03-29 17:07:54 +0300

    lower fixed keyword match

--- main_hired.py
+++ main_hired.py
@@ -71,7 +71,7 @@ with data_and_selection:
         flat_list =[]
         for skill in flat_listt: 
             skill = skill.strip()
-            if skill != keyword.lower():
+            if skill.lower() != keyword.lower():
                 flat_list.append(skill)
         myDict = Counter(flat_list)
 
